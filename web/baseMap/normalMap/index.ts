b43424ea16e8f658c873abc959c5201948a6ac19
metsman <swine777@gmail.com>
2023-11-14 16:30:42 +0900

    최초올림

---
+++ web/baseMap/normalMap/index.ts
@@ -0,0 +1,11 @@
+import ktGms from "kt-map-sdk-js";
+
+let map = new ktGms.Map({
+    container: "map",
+    style: "normal",
+    center: [127.017422, 37.49144],
+    zoom: 16,
+    maxPitch: 68
+});
+
+//좋지뭐!! Mets
