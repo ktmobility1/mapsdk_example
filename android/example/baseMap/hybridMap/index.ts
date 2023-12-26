8432218b5ccba4c0ed98163f2c3b52946c3b22a6
ktmobility1 <ktmobility1@gmail.com>
2023-12-26 12:27:30 +0900

    [Update] android base example

--- android/example/baseMap/hybridMap/index.ts
+++
@@ -1,9 +0,0 @@
-import ktGms from "kt-map-sdk-js";
-
-let map = new ktGms.Map({
-    container: "map",
-    style: "hybrid",
-    center: [127.017422, 37.49144],
-    zoom: 16,
-    maxPitch: 68
-});
