5b8ba4c7a85b918f7d90b26d410f78fc5907dd81
Jongbin Lee <jongbin.lee@kt.com>
2024-03-08 14:32:57 +0900

    [Update] Web GeoAnalysis example

--- web/example/geoanalysisService/center_js/index.js
+++ web/example/geoanalysisService/center_js/index.js
@@ -1,12 +1,12 @@
-import ktGms from "kt-map-sdk-js"
-import analysis from "kt-map-sdk-geoanalysis"
+import ktGms from "kt-map-sdk-js";
+import analysis from "kt-map-sdk-geoanalysis";
 let map = new ktGms.Map({
   container: "map",
   style: "normal",
   center: [127.017422, 37.49144],
   zoom: 15,
-  maxPitch: 68
-})
+  maxPitch: 68,
+});
 
 map.on("load", () => {
   // MultiPoint 생성
@@ -14,36 +14,36 @@ map.on("load", () => {
     [
       [127.02222, 37.49144],
       [127.015522, 37.49044],
-      [127.015522, 37.49294]
+      [127.015522, 37.49294],
     ],
     {}
-  )
-  const centerPoint = analysis.measurement.center(multiPoint)
+  );
+  const centerPoint = analysis.measurement.center(multiPoint);
   // 지도에 Point 객체 및 PointLayer 추가
   new ktGms.layer.PointLayer( //Layer ID
     "point_layer", //PointLayer에 적용할 스타일
     new ktGms.style.CircleStyle(
       {
         "circle-radius": 10,
-        "circle-color": "#000000"
+        "circle-color": "#000000",
       },
       {
-        visibility: "visible"
+        visibility: "visible",
       }
     ),
     multiPoint
-  ).addTo(map)
+  ).addTo(map);
   new ktGms.layer.PointLayer( //Layer ID
     "center_point_layer", //PointLayer에 적용할 스타일
     new ktGms.style.CircleStyle(
       {
         "circle-radius": 10,
-        "circle-color": "#FF0000"
+        "circle-color": "#FF0000",
       },
       {
-        visibility: "visible"
+        visibility: "visible",
       }
     ),
     new ktGms.source.GeoJSONSource("center_point", { data: centerPoint })
-  ).addTo(map)
-})
+  ).addTo(map);
+});
